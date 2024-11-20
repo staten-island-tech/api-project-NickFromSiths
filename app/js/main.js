0ff12439e0b35c1fde065fe098cb0c5c77784e2c
Nick <nmalyarchuk07@gmail.com>
2024-11-20 14:00:44 -0500

    .

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,21 @@
 import "../css/style.css";
+
+async function getData() {
+  //return a promise
+  try {
+    const response = await fetch(url, { method: "GET", headers });
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      //convert promise to json
+      const data = await response.json();
+      console.log(data);
+      //unique to THIS API
+    }
+  } catch (error) {
+    console.error("Does not work");
+  }
+}
+getData();
+
+console.log(response);
